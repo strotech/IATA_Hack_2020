d86e9d0b0ac91ec0150c09863f426c4d454ca410
iCargo <icargo@Administrators-MacBook-Pro.local>
2020-09-06 20:54:24 +0530

    Initial Commit

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -10,7 +10,7 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
     appBar:AppBar(
-      title:Text('eCommerce Portal',
+      title:Text('AirCargo eCommerce Portal',
       style:TextStyle(fontSize: 20.0,
         fontWeight: FontWeight.bold,
         letterSpacing: 2.0,
