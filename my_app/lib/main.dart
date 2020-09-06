c2ff6dda915edf6f38a81871d5b014fdfba107d7
iCargo <icargo@Administrators-MacBook-Pro.local>
2020-09-06 20:45:24 +0530

    Initial Commit

---
+++ my_app/lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Home()
+));
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    appBar:AppBar(
+      title:Text('eCommerce Portal',
+      style:TextStyle(fontSize: 20.0,
+        fontWeight: FontWeight.bold,
+        letterSpacing: 2.0,
+        // color: Colors.black,
+        // fontFamily: 'IndieFlower'
+        )),
+      centerTitle: true,
+      backgroundColor: Colors.blue[900],
+    ),
+    body:SafeArea(
+      child:Container(
+        decoration: BoxDecoration(
+                  image:DecorationImage(
+                    image:AssetImage('assets/images/wing.jpg'),
+                    fit:BoxFit.cover
+                  )
+                ),
+                child:Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      RaisedButton.icon(onPressed: (){}, icon: Icon(Icons.camera), label: Text("Capture Cargo"),
+                      color: Colors.blue[900],
+                      textColor: Colors.white,
+                      ),
+                      // RaisedButton.icon(onPressed: null, icon: Icon(Icons.mail), label: Text("Capture Cargo"),
+                      // color: Colors.blue,
+                      // )
+                    ],
+                  ),
+                )
+      ),
+      
+    ),
+    floatingActionButton: FloatingActionButton(
+      child:Text("About",
+      style:TextStyle(
+        // fontFamily: 'IndieFlower'
+      )),
+      onPressed: (null),
+      backgroundColor: Colors.blue[900],
+      // foregroundColor: Colors.grey[600],
+    ),
+  );
+  }
+}
